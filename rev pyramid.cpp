643521f52cc29ddc434893f7204c73c9b804ca91
Ananya Patni <patni.ananya.2531@gmail.com>
2025-07-04 21:04:46 +0530

    Update and rename rev pyramid to rev pyramid.cpp

--- rev pyramid.cpp
+++ rev pyramid.cpp
@@ -4,8 +4,9 @@ int main() {
     int i, j;
     for (i = 5; i > 0; i--) {
         for (j = 0; j < i; j++) {
-            cout << "*";
+            cout << "%d", &i;
         }
         cout << endl;
     }
 }
+//to print a reverse number pyramid using nested for loops
