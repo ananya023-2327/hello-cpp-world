7f971bd998c2aa3402f9c151c03f2be49a3bf3b4
Ananya Patni <patni.ananya.2531@gmail.com>
2025-07-12 01:12:30 +0530

    Create pyramid.cpp

---
+++ pyramid.cpp
@@ -0,0 +1,28 @@
+// pyramid_pattern.cpp
+// This program generates a classic pyramid pattern using asterisks.
+
+#include <iostream>
+
+int main() {
+    int rows;
+
+
+    std::cout << "Enter the number of rows for the pyramid: ";
+    std::cin >> rows;
+
+
+    for (int i = 1; i <= rows; ++i) {
+       
+        for (int space = 1; space <= rows - i; ++space) {
+            std::cout << " ";
+        }
+     
+        for (int j = 1; j <= 2 * i - 1; ++j) {
+            std::cout << "*";
+        }
+       
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
